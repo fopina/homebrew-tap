307cabf024aace550dfa284c0ab9873d06be5123
Filipe Pina <636320+fopina@users.noreply.github.com>
2020-12-06 01:16:35 +0000

    Update git-group.rb

--- Formula/git-group.rb
+++ Formula/git-group.rb
@@ -4,6 +4,7 @@ class GitGroup < Formula
   url "https://github.com/fopina/git-group/releases/download/v0.0.8/git-group_0.0.8_darwin_amd64.tar.gz"
   sha256 "64ab5fcea620d445c3ed4ee3c179bac29567ca4712ddb9c443196d0074c26ef1"
   license "MIT"
+  version "0.0.8"
 
   def install
     bin.install "git-group"

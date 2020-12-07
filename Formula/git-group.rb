d867ad2e138a5516a0df0166b04bd39014f2af27
Filipe Pina <fopina@gmail.com>
2020-12-07 01:46:42 +0000

    git-group 0.0.9

--- Formula/git-group.rb
+++ Formula/git-group.rb
@@ -1,16 +1,16 @@
 class GitGroup < Formula
   desc "Easily clone all the repositories from a group or organization"
-  homepage "https://github.com/fopina/git-group/"
-  url "https://github.com/fopina/git-group/releases/download/v0.0.8/git-group_0.0.8_darwin_amd64.tar.gz"
-  sha256 "64ab5fcea620d445c3ed4ee3c179bac29567ca4712ddb9c443196d0074c26ef1"
-  license "MIT"
-  version "0.0.8"
+  homepage "https://github.com/fopina/git-group"
+  url "https://github.com/fopina/git-group/releases/download/v0.0.9/git-group_0.0.9_darwin_amd64.tar.gz"
+  sha256 "7c62077f52b201b0c493d19dc90344712702caf3e5bb427a0c3b02c6dce52499"
+  license "https://github.com/fopina/git-group/releases/download/v0.0.9/git-group_0.0.9_darwin_amd64.tar.gz"
+  version "0.0.9"
 
   def install
     bin.install "git-group"
   end
   
   test do
-    assert_match "0.0.8", shell_output("#{bin}/git-group -v 2>&1", 0)
+    assert_match "0.0.9", shell_output("#{bin}/git-group -v 2>&1", 0)
   end
 end

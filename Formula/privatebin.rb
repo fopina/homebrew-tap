8815cf3223a7a724e5305648097f78bad05ded51
Filipe Pina <636320+fopina@users.noreply.github.com>
2020-12-07 02:21:48 +0000

    Update privatebin.rb

--- Formula/privatebin.rb
+++ Formula/privatebin.rb
@@ -1,7 +1,7 @@
 class Privatebin < Formula
   desc "PrivateBin CLI written in Golang"
   homepage "https://github.com/fopina/privatebin"
-  url "https://github.com/fopina/privatebin/releases/download/0.2.2/pbin_darwin_x86_64.tar.gz"
+  url "https://github.com/fopina/privatebin/releases/download/v0.2.2/pbin_darwin_x86_64.tar.gz"
   sha256 "ed84dcdeada8f9e0820ce62d3a4711a681ce24adb4e138b082f7b589cd0586e3"
   license ""
   version "0.2.2"

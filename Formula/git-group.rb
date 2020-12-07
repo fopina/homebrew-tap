cf3cd10b4e092c152ff5674ef236e051d018a102
Filipe Pina <fopina@gmail.com>
2020-12-07 01:58:19 +0000

    git-group 0.0.9

--- Formula/git-group.rb
+++ Formula/git-group.rb
@@ -2,8 +2,8 @@ class GitGroup < Formula
   desc "Easily clone all the repositories from a group or organization"
   homepage "https://github.com/fopina/git-group"
   url "https://github.com/fopina/git-group/releases/download/v0.0.9/git-group_0.0.9_darwin_amd64.tar.gz"
-  sha256 "7c62077f52b201b0c493d19dc90344712702caf3e5bb427a0c3b02c6dce52499"
-  license "https://github.com/fopina/git-group/releases/download/v0.0.9/git-group_0.0.9_darwin_amd64.tar.gz"
+  sha256 "6b52fb9c1e1198c41a96c8ee3b63f71f94d5645a37e36f97cf41967e0b2508e4"
+  license "MIT"
   version "0.0.9"
 
   def install
